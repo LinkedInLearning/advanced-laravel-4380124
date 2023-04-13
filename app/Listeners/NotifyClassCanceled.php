ff8fd9f9b3dd2ffe107f4d50c401ad785bc88d50
Thirus <thirusofficial@gmail.com>
2023-04-13 11:59:09 +0530

    Send mails

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -3,7 +3,9 @@
 namespace App\Listeners;
 
 use App\Events\ClassCanceled;
+use App\Mail\ClassCanceledMail;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -22,7 +24,15 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $scheduledClass = $event->scheduledClass;
-        Log::info($scheduledClass);
+        $members = $event->scheduledClass->members();
+
+        $className = $event->scheduledClass->classType->name;
+        $classDateTime = $event->scheduledClass->date_time;
+
+        $details = compact('className','classDateTime');
+
+        $members->each(function($user) use ($details){
+            Mail::to($user)->send(new ClassCanceledMail($details));
+        });
     }
 }
---
+++ resources/views/emails/class-canceled.blade.php
@@ -0,0 +1,3 @@
+<p>Hey,</p>
+<p>Sorry, your {{ $details['className'] }} class on {{ $details['classDateTime']->format('jS F') }} at {{ $details['classDateTime']->format('g:i a') }}
+    was canceled by the instructor. Check the schedule and book another. Thank you</p>
