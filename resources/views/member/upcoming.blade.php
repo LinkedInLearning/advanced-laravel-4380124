06c1a3f28196f12e4043c6a11a989bc2c04d1a92
Thirus <thirusofficial@gmail.com>
2023-04-13 10:40:39 +0530

    Minor change in padding in views

--- resources/views/member/upcoming.blade.php
+++ resources/views/member/upcoming.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 max-w-2xl divide-y">
+                <div class="p-10 text-gray-900 max-w-2xl divide-y">
                     @forelse ($bookings as $class)
                   <div class="py-6">
                      <div class="flex gap-6 justify-between">
