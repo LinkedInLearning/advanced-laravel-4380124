ae9648f71bab77f340fdc91559ea586576b563c6
Thirus <thirusofficial@gmail.com>
2023-04-14 11:27:48 +0530

    Write database tests

--- tests/Feature/InstructorTest.php
+++ tests/Feature/InstructorTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\ClassType;
+use App\Models\ScheduledClass;
 use Tests\TestCase;
 use App\Models\User;
 use Database\Seeders\ClassTypeSeeder;
@@ -11,6 +12,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class InstructorTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_instructor_is_redirected_to_instructor_dashboard() {
         $user = User::factory()->create([
             'role' => 'instructor'
@@ -40,6 +43,33 @@ class InstructorTest extends TestCase
         ]);
 
         //Then 
+
+        $this->assertDatabaseHas('scheduled_classes',[
+            'class_type_id' => ClassType::first()->id,
+            'date_time' => '2023-04-20 09:00:00'
+        ]);
         $response->assertRedirectToRoute('schedule.index');
+    }
+
+    public function test_instructor_can_cancel_class() {
+        // Given
+        $user = User::factory()->create([
+            'role' => 'instructor'
+        ]);
+        $this->seed(ClassTypeSeeder::class);
+        $scheduledClass = ScheduledClass::create([
+            'instructor_id' => $user->id,
+            'class_type_id' => ClassType::first()->id,
+            'date_time' => '2023-04-20 10:00:00'
+        ]);
+
+        // When
+        $response = $this->actingAs($user)
+            ->delete('/instructor/schedule/'.$scheduledClass->id);
+
+        // Then
+        $this->assertDatabaseMissing('scheduled_classes',[
+            'id' => $scheduledClass->id
+        ]);
     }
 }
